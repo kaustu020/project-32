d79d309eec5499f6d4fc68ea03a6b2860eb85229
kaustu020 <81802576+kaustu020@users.noreply.github.com>
2021-04-12 18:44:39 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,48 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var backgroundImg;
+
+var bg ;
+
+function preload() {
+    // create getBackgroundImg( ) here
+}
+
+function setup(){
+    var canvas = createCanvas(1200,700);
+    engine = Engine.create();
+    world = engine.world;
+
+}
+
+function draw(){
+
+    // add condition to check if any background image is there to add
+
+
+    Engine.update(engine);
+
+    // write code to display time in correct format here
+
+
+}
+
+async function getBackgroundImg(){
+
+    // write code to fetch time from API
+var response = await fetch("write API url here")
+    //change the data in JSON format
+hours = datetime.slice(11,13);
+    // write code slice the datetime
+
+
+    // add conditions to change the background images from sunrise to sunset
+
+
+    //load the image in backgroundImg variable here
+backgroundImg=loadImage(bg)
+}
